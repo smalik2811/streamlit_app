8cd1f3488c42c499fcd629a82bfbeb1d0df88f02
SONU SINGH <68186347+smalik2811@users.noreply.github.com>
2023-12-07 19:27:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ main_column, result_column = st.columns(2, gap="medium")
 
 with main_column:
     with st.container(border=True):
-        num_1 = st.number_input("Enter the Numero Uno of your trio", min_value=0, step=1)
-        num_2 = st.number_input("Contribute the Second Symphony of Digits", min_value=0, step=1)
-        num_3 = st.number_input("Complete the Trinity with your Third Marvelous Digit",
-                                   min_value=0, step=1)
+        num_1 = st.number_input("Enter the Numero Uno of your trio", step=1)
+        num_2 = st.number_input("Contribute the Second Symphony of Digits", step=1)
+        num_3 = st.number_input("Complete the Trinity with your Third Marvelous Digit", step=1)
 
 def find_greatest(num_1, num_2, num_3):
     return max(num_1, num_2, num_3)
@@ -21,7 +20,7 @@ def show_result():
     greatest_num = find_greatest(num_1, num_2, num_3)
     st.write("#### Behold the Champion: The Supreme Number is Revealed!")
     with st.container(border = True):
-        st.write("# {}".format(greatest_num))
+        st.write("# :green[{}]".format(greatest_num))
 
 with result_column:
     if st.button("Ascend to Greatness!", type="primary"):
