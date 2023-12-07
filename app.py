b3dd9609ad4f0cb12451c82b74ef12e57f7fffb4
SONU SINGH <68186347+smalik2811@users.noreply.github.com>
2023-12-07 18:40:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.subheader("Discover the Ascendancy of Numerical Dominance :sunglasses:", divi
 
 st.write("### Unleash the Magic of Three! Enter Your Sacred Numbers:")
 
-main_column, img_column = st.columns(2, gap = "medium")
+main_column, result_column = st.columns(2, gap = "medium")
 
 with main_column:
   with st.container(border = True):
@@ -13,5 +13,5 @@ with main_column:
     num_2 = st.number_input("Contribute the Second Symphony of Digits", min_value = 0, step = 1)
     num_3 = st.number_input("Complete the Trinity with your Third Marvelous Digit", min_value = 0, step = 1)
 
-with img_column:
-  st.write("result here")
+with result_column:
+  st.button("Ascend to Greatness!")
