32c341d2d9561bff73b983fde287965256fdc672
SONU SINGH <68186347+smalik2811@users.noreply.github.com>
2023-12-07 18:56:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 import streamlit as st
 
-st.header("Triumphant Trio: :blue[Unveiling the Mighty Number!]")
-st.subheader("Discover the Ascendancy of Numerical Dominance :sunglasses:", divider='rainbow')
+st.header("Triumphant Trio: **Unveiling the Mighty Number!**")
+st.subheader("Discover the Ascendancy of Numerical Dominance :sunglasses:")
 
-st.write("### Unleash the Magic of Three! Enter Your Sacred Numbers:")
+st.markdown("### Unleash the Magic of Three! Enter Your Sacred Numbers:")
 
-main_column, result_column = st.columns(2, gap="medium")
+main_column, result_column = st.columns(2)
 
 with main_column:
-    with st.container(border=True):
-        num_1 = st.number_input("Enter the Numero Uno of your trio", min_value=0, step=1)
-        num_2 = st.number_input("Contribute the Second Symphony of Digits", min_value=0, step=1)
-        num_3 = st.number_input("Complete the Trinity with your Third Marvelous Digit",
-                                   min_value=0, step=1)
+    num_1 = st.number_input("Enter the Numero Uno of your trio", min_value=0, step=1)
+    num_2 = st.number_input("Contribute the Second Symphony of Digits", min_value=0, step=1)
+    num_3 = st.number_input("Complete the Trinity with your Third Marvelous Digit", min_value=0, step=1)
 
 def find_greatest(num_1, num_2, num_3):
     return max(num_1, num_2, num_3)
 
-if st.button("Ascend to Greatness!", type="primary"):
+if st.button("Ascend to Greatness!"):
     greatest_num = find_greatest(num_1, num_2, num_3)
-    st.write("#### Behold the Champion: The Supreme Number is Revealed!")
+    st.markdown("#### Behold the Champion: The Supreme Number is Revealed!")
     st.write(greatest_num)
