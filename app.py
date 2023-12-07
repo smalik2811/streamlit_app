184ce627d908bc1eab16af1f27892cf09fcb7e1d
SONU SINGH <68186347+smalik2811@users.noreply.github.com>
2023-12-07 17:10:59 +0530

    Update app.py
    Added few parts

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 
-st.write("""
-# Triumphant Trio: Unveiling the Mighty Number!
-## Discover the Ascendancy of Numerical Dominance
-""")
+st.header("Triumphant Trio: :blue[Unveiling the Mighty Number!]")
+st.subheader("Discover the Ascendancy of Numerical Dominance :sunglasses:", divider = 'rainbow)
 
-st.header("Unleash the Magic of Three! Enter Your Sacred Numbers.")
+main_column, img_column = st.columns(2, gap = "medium")
 
-num_1 = st.number_input("Enter the Numero Uno of your trio", min_value = 0, step = 1)
-num_2 = st.number_input("Contribute the Second Symphony of Digits", min_value = 0, step = 1)
-num_3 = st.number_input("Complete the Trinity with your Third Marvelous Digit", min_value = 0, step = 1)
+with main_column:
+  st.text("Unleash the Magic of Three! Enter Your Sacred Numbers.")
+
+  num_1 = st.number_input("Enter the Numero Uno of your trio", min_value = 0, step = 1)
+  num_2 = st.number_input("Contribute the Second Symphony of Digits", min_value = 0, step = 1)
+  num_3 = st.number_input("Complete the Trinity with your Third Marvelous Digit", min_value = 0, step = 1)
+
+with img_column:
+  st.write("result here")
+  st.image("url")
