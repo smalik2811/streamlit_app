2d3371dcfea4e33615ae4eab1fa127beeb59d098
SONU SINGH <68186347+smalik2811@users.noreply.github.com>
2023-12-07 18:36:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ st.write("### Unleash the Magic of Three! Enter Your Sacred Numbers:")
 main_column, img_column = st.columns(2, gap = "medium")
 
 with main_column:
-  with st.container():
+  with st.container(border = True):
     num_1 = st.number_input("Enter the Numero Uno of your trio", min_value = 0, step = 1)
     num_2 = st.number_input("Contribute the Second Symphony of Digits", min_value = 0, step = 1)
     num_3 = st.number_input("Complete the Trinity with your Third Marvelous Digit", min_value = 0, step = 1)
 
 with img_column:
   st.write("result here")
-  st.image("https://gramener.com/comicgen/v1/comic?name=aavatar&gender=female&character=bindi&facestyle=sketchy&emotion=lookingleft&attire=saree&pose=angry&face=%23f9e6c8&shirt=%23bcd1ec&pant=%233a4e5c&box=&boxcolor=%23000000&boxgap=100&mirror=", use_column_width="always")
